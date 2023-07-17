d87721ad22ccaf1bacc2ddce8b1e2423a41465bb
Robson Matos <nosbor_album@hotmail.com>
2023-07-17 20:28:11 -0300

    Corrigindo bug no js

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+    const addTaskButton = document.getElementById('addTaskButton');
+
+    addTaskButton.addEventListener('click', function() {
+      //O trim() é um método de string em JavaScript que remove quaisquer espaços em branco no início e no final da string
+      const taskText = taskInput.value.trim();
+
+      if(taskText !== ''){
+        const taskItem = document.createElement('li');//cria um elemento do tipo li
+        taskItem.textContent = taskText;
+
+        const deleteButton = document.createElement('button');// cria um elemento do tipo botton
+        deleteButton.textContent = 'Excluir';
+
+        taskItem.appendChild(deleteButton);
+        taskList.appendChild(taskItem);
+
+        deleteButton.addEventListener('click', function(){
+          taskItem.remove();
+        });
+        taskInput.value = '';
+      }
+    });
+  });
+
+
+
+
+
+
+
+/*document.addEventListener('DOMContentLoaded', function() {
 
   const taskInput = document.getElementbyId('taskInput');
   const taskList = document.getElementbyId('taskList');
   const addTaskButton = document.getElementbyId('addTaskbutton');
 
   addTaskButton.addEventListener('click', function() {
-    //O trim() é um método de string em JavaScript que remove quaisquer espaços em branco no início e no final da string
+    
     const taskText = taskInput.value.trim(); //
 
     if(taskText !== ''){
-      const taskItem = document.createElement('li'); // cria um elemento do tipo li
+      const taskItem = document.createElement('li'); // 
       taskItem.textContent = taskText;
 
-      const deleteButton = document.createElement('button'); // cria um elemento do tipo botton
+      const deleteButton = document.createElement('button'); 
 
       taskItem.oppendChild(deleteButton);
       taskList.oppendChild(taskItem);
@@ -24,9 +57,5 @@ document.addEventListener('DOMContentLoaded', function() {
       taskInput.value = '';
     }
   });
-});
+});*/
 
-const divElement = document.querySelector('#container'); // Seleciona a div desejada
-
-divElement.style.display = 'flex'; // Configura a div para usar um layout flexível
-divElement.style.justifyContent = 'center'; // Centraliza horizontalmente o conteúdo da div
