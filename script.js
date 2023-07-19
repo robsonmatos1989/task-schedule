03e221f4c74a61e9a7f5096249d9b8085417b9c9
Robson Matos <nosbor_album@hotmail.com>
2023-07-18 22:24:39 -0300

    Adicionado função para a execução do temporizador

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     const taskInput = document.getElementById('taskInput');
     const taskList = document.getElementById('taskList');
     const addTaskButton = document.getElementById('addTaskButton');
@@ -23,5 +23,53 @@ document.addEventListener('DOMContentLoaded', function() {
         taskInput.value = '';
       }
     });
+  });*/
+
+document.addEventListener('DOMContentLoaded', function() {
+  const taskInput = document.getElementById('taskInput');
+  const taskList = document.getElementById('taskList');
+  const taskTimer = document.getElementById( 'taskTimer');
+  const addTaskButton = document.getElementById('addTaskButton');
+
+  addTaskButton.addEventListener('click', function() {
+    const taskText = taskInput.value.trim();
+    const timerValue = parseInt(taskTimer.value);
+
+    if (taskText !== '') {
+      const taskItem = document.createElement('li');
+      taskItem.textContent = taskText;
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = ' Excluir';
+
+
+      /*const taskInput = document.createElement('input');
+      taskInput.type = 'number';
+      taskInput.placeholder = 'Digite o tempo em minutos';
+
+      taskItem.appendChild(taskInput);*/
+      taskItem.appendChild(deleteButton);
+      taskList.appendChild(taskItem);
+
+      deleteButton.addEventListener('click', function() {
+        taskItem.remove();
+      });
+
+      taskInput.value = '';
+      taskTimer.value = '';
+
+      // Define um temporizador de 1 minuto para exibir o alerta
+      setTimeout(function() {
+
+        showAlert(taskText);
+      }, timerValue * 60 * 1000); // 1 minuto em milissegundos
+    }
   });
+
+  // Função para exibir o alerta
+  function showAlert(taskText) {
+    alert('Lembrete: ' + taskText);
+  }
+
+});
 
