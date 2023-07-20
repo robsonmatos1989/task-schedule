9dfe7a3a8c4a8579a0d30676ff8118ba6f2ee278
Robson Matos <nosbor_album@hotmail.com>
2023-07-19 21:46:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
-/*document.addEventListener('DOMContentLoaded', function() {
-    const taskInput = document.getElementById('taskInput');
-    const taskList = document.getElementById('taskList');
-    const addTaskButton = document.getElementById('addTaskButton');
-
-    addTaskButton.addEventListener('click', function() {
-      //O trim() é um método de string em JavaScript que remove quaisquer espaços em branco no início e no final da string
-      const taskText = taskInput.value.trim();
-
-      if(taskText !== ''){
-        const taskItem = document.createElement('li');//cria um elemento do tipo li
-        taskItem.textContent = taskText;
-
-        const deleteButton = document.createElement('button');// cria um elemento do tipo botton
-        deleteButton.textContent = 'Excluir';
-
-        taskItem.appendChild(deleteButton);
-        taskList.appendChild(taskItem);
-
-        deleteButton.addEventListener('click', function(){
-          taskItem.remove();
-        });
-        taskInput.value = '';
-      }
-    });
-  });*/
-
+/*
 document.addEventListener('DOMContentLoaded', function() {
   const taskInput = document.getElementById('taskInput');
   const taskList = document.getElementById('taskList');
@@ -43,11 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
       deleteButton.textContent = ' Excluir';
 
 
-      /*const taskInput = document.createElement('input');
-      taskInput.type = 'number';
-      taskInput.placeholder = 'Digite o tempo em minutos';
-
-      taskItem.appendChild(taskInput);*/
+      taskItem.appendChild(taskInput);
       taskItem.appendChild(deleteButton);
       taskList.appendChild(taskItem);
 
@@ -58,11 +28,11 @@ document.addEventListener('DOMContentLoaded', function() {
       taskInput.value = '';
       taskTimer.value = '';
 
-      // Define um temporizador de 1 minuto para exibir o alerta
+      // Define um temporizador para exibir o alerta
       setTimeout(function() {
 
         showAlert(taskText);
-      }, timerValue * 60 * 1000); // 1 minuto em milissegundos
+      }, timerValue * 60 * 1000); 
     }
   });
 
@@ -71,5 +41,76 @@ document.addEventListener('DOMContentLoaded', function() {
     alert('Lembrete: ' + taskText);
   }
 
+});
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+  const taskInput = document.getElementById('taskInput');
+  const taskList = document.getElementById('taskList');
+  const taskTimer = document.getElementById('taskTimer');
+  const addTaskButton = document.getElementById('addTaskButton');
+
+  addTaskButton.addEventListener('click', function() {
+    const taskText = taskInput.value.trim();
+    const timerValue = parseInt(taskTimer.value);
+
+    if (taskText !== '') {
+      const taskItem = document.createElement('li');
+      taskItem.textContent = taskText;
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Excluir';
+
+      taskItem.appendChild(deleteButton);
+      taskList.appendChild(taskItem);
+
+      deleteButton.addEventListener('click', function() {
+        taskItem.remove();
+        saveTasksToLocalStorage();
+      });
+
+      taskInput.value = '';
+      taskTimer.value = '';
+
+      saveTasksToLocalStorage();
+
+      setTimeout(function() {
+        showAlert(taskText);
+      }, timerValue * 60 * 1000);
+    }
+  });
+
+  function showAlert(taskText) {
+    alert('Lembrete: ' + taskText);
+  }
+
+  function saveTasksToLocalStorage() {
+    const tasks = Array.from(taskList.getElementsByTagName('li')).map(taskItem => taskItem.textContent);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  function loadTasksFromLocalStorage() {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (tasks) {
+      tasks.forEach(taskText => {
+        const taskItem = document.createElement('li');
+        taskItem.textContent = taskText;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Excluir';
+
+        deleteButton.addEventListener('click', function() {
+          taskItem.remove();
+          saveTasksToLocalStorage();
+        });
+
+        taskItem.appendChild(deleteButton);
+        taskList.appendChild(taskItem);
+      });
+    }
+  }
+
+  // Chamar a função loadTasksFromLocalStorage após a definição dos elementos
+  loadTasksFromLocalStorage();
 });
 
