4d791d992983f2a0cf73efec388615d24ece283f
Robson Matos <nosbor_album@hotmail.com>
2023-07-20 22:07:10 -0300

    Commit

--- script.js
+++ script.js
@@ -9,27 +9,32 @@ document.addEventListener('DOMContentLoaded', function() {
     const timerValue = parseInt(taskTimer.value);
 
     if (taskText !== '') {
+      const taskId = generateTaskId();
+
       const taskItem = document.createElement('li');
 
+      const taskContainer = document.createElement('div'); // Elemento para conter o texto da tarefa e o botão
+      taskItem.appendChild(taskContainer);
+
       const taskTextSpan = document.createElement('span');
-      taskTextSpan.textContent = taskText + ' '; // Adicionando espaço em branco após o texto
-      taskItem.appendChild(taskTextSpan);
+      taskTextSpan.textContent = taskText;
+      taskContainer.appendChild(taskTextSpan);
 
       const deleteButton = document.createElement('button');
       deleteButton.textContent = 'Excluir';
 
-      taskItem.appendChild(deleteButton);
+      taskContainer.appendChild(deleteButton);
       taskList.appendChild(taskItem);
 
       deleteButton.addEventListener('click', function() {
         taskItem.remove();
-        saveTasksToLocalStorage();
+        removeTaskFromLocalStorage(taskId);
       });
 
       taskInput.value = '';
       taskTimer.value = '';
 
-      saveTasksToLocalStorage();
+      saveTaskToLocalStorage({ id: taskId, text: taskText });
 
       setTimeout(function() {
         showAlert(taskText);
@@ -41,30 +46,44 @@ document.addEventListener('DOMContentLoaded', function() {
     alert('Lembrete: ' + taskText);
   }
 
-  function saveTasksToLocalStorage() {
-    const tasks = Array.from(taskList.getElementsByTagName('li')).map(taskItem => taskItem.textContent);
+  function saveTaskToLocalStorage(task) {
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.push(task);
     localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  function removeTaskFromLocalStorage(taskId) {
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks = tasks.filter(task => task.id !== taskId);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  function generateTaskId() {
+    return Date.now().toString(36);
   }
 
   function loadTasksFromLocalStorage() {
     const tasks = JSON.parse(localStorage.getItem('tasks'));
     if (tasks) {
-      tasks.forEach(taskText => {
+      tasks.forEach(task => {
         const taskItem = document.createElement('li');
 
+        const taskContainer = document.createElement('div'); // Elemento para conter o texto da tarefa e o botão
+        taskItem.appendChild(taskContainer);
+
         const taskTextSpan = document.createElement('span');
-        taskTextSpan.textContent = taskText + ' '; // Adicionando espaço em branco após o texto
-        taskItem.appendChild(taskTextSpan);
+        taskTextSpan.textContent = task.text;
+        taskContainer.appendChild(taskTextSpan);
 
         const deleteButton = document.createElement('button');
         deleteButton.textContent = 'Excluir';
 
         deleteButton.addEventListener('click', function() {
           taskItem.remove();
-          saveTasksToLocalStorage();
+          removeTaskFromLocalStorage(task.id);
         });
 
-        taskItem.appendChild(deleteButton);
+        taskContainer.appendChild(deleteButton);
         taskList.appendChild(taskItem);
       });
     }
